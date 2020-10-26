7482371c9429e8a71679994e1e8ffde3ff287601
kd ang <kdang82590@gmail.com>
2020-10-25 23:42:14 -0500

    dice game

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -9,15 +10,49 @@ void main() {
           title: Text('Dicee'),
           backgroundColor: Colors.red,
         ),
-        body: DicePage(),
+        body: Dice(),
       ),
     ),
   );
 }
 
-class DicePage extends StatelessWidget {
+class Dice extends StatefulWidget {
+  @override
+  DicePage createState() => DicePage();
+}
+
+class DicePage extends State<StatefulWidget> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 2;
+
+  diceNumber(){
+       setState(() {
+         leftDiceNumber = Random().nextInt(6) + 1;
+         rightDiceNumber = Random().nextInt(6) + 1;
+       });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                    diceNumber();
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png')),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                 diceNumber();
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png')),
+          )
+        ],
+      ),
+    );
   }
 }
